87909d5b53ce4650d12707893c1d3f0b347c38ba
Fishai <kyleaubreyford@gmail.com>
2018-12-07 09:55:52 -0600

    cognito implementation part1

--- src/main/java/com/revature/models/CognitoRegister.java
+++ src/main/java/com/revature/models/CognitoRegister.java
@@ -3,7 +3,6 @@ package com.revature.models;
 public class CognitoRegister {
 
 	private String Username;
-	private List<Attribute> Attributes;
 	private String UserCreateData;
 	private boolean UserLastModifiedData;
 	public String Enabled;
