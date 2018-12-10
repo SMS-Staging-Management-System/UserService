a21ec95281f8763e2a5cf24ba107646c9010f713
Daniel Stine <daniel.stine@me.com>
2018-12-10 11:17:46 -0600

    Added support for Swagger UI to the UsersService

--- src/main/java/com/revature/UserServiceApplication.java
+++ src/main/java/com/revature/UserServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,8 +19,10 @@ public class UserServiceApplication {
 
 	@Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2)
+        		.select()
+        		.apis(RequestHandlerSelectors.basePackage("com.revature.controllers"))
+                .build();
     }
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
