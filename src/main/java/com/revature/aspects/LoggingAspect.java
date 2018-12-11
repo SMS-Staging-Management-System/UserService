e4c6f4bb1cd5163411d9e1355296ae7245e468c0
Daniel Stine <daniel.stine@me.com>
2018-12-11 14:27:33 -0600

    Added logging to the UsersService

---
+++ src/main/java/com/revature/aspects/LoggingAspect.java
@@ -0,0 +1,33 @@
+package com.revature.aspects;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+//import com.revature.annotations.Logging;
+
+@Aspect
+@Component
+public class LoggingAspect {
+	private Logger logger = Logger.getRootLogger();
+	
+	@Around("execution(* com.revature.services.*.*(..))")
+	public Object Logging(ProceedingJoinPoint pjp) throws Throwable {
+		String logInfo = "";
+		logInfo = logInfo + "Method: " + pjp.getSignature().getName() + "\n";
+		String args = "";
+		if (pjp.getArgs().length > 0) {
+			args = "Arguments:";
+			for (Object o: pjp.getArgs()) {
+				args = args + " " + o.toString();
+			}
+			logInfo = logInfo + args + "\n";
+		}
+		Object value = pjp.proceed();
+		logInfo = logInfo + "Result: " + value.toString() + "\n";
+		logger.info(logInfo);
+		return value;
+	}
+}
