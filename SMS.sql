7da9d4d75f2bb24e4cf95ffda27e30fedba7553e
Josh S <jhstoltenberg@gmail.com>
2018-12-14 09:28:17 -0600

    Update SMS.sql
    edit this script to reflect the current state of the database to
    facilitate the transition into production

--- SMS.sql
+++ SMS.sql
@@ -1,4 +1,4 @@
-SET SCHEMA 'SMS_Users';
+SET SCHEMA 'sms';
 
 CREATE TABLE roles
 (
@@ -14,10 +14,13 @@ CREATE TABLE sms_users
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     username TEXT NOT NULL UNIQUE,
-    user_role INTEGER NOT NULL,
+    phone_number TEXT,
+    country TEXT,
+    timezone TEXT,
+    zip_code TEXT,
+    city TEXT,
+    state TEXT,
     CONSTRAINT sms_users_PK PRIMARY KEY (sms_user_id),
-    CONSTRAINT sms_users_FK_role FOREIGN KEY (user_role)
-    REFERENCES roles (role_id) ON DELETE CASCADE
 );
 
 CREATE TABLE cohorts 
@@ -26,7 +29,10 @@ CREATE TABLE cohorts
     cohort_name TEXT NOT NULL UNIQUE,
     cohort_description TEXT NOT NULL,
     cohort_token TEXT NOT NULL,
-    CONSTRAINT sms_cohorts_PK PRIMARY KEY (cohort_id)
+    trainer_id INTEGER NOT NULL,
+    CONSTRAINT sms_cohorts_PK PRIMARY KEY (cohort_id),
+    CONSTRAINT sms_cohorts_FK_trainer FOREIGN KEY (trainer_id)
+    REFERENCES sms_users (sms_user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE users_cohorts
