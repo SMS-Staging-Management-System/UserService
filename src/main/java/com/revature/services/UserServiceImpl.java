92d140754d02697a658a179d7b6bf00fd6c8bc0b
MidoOmar95 <mohamedwomar21@gmail.com>
2019-03-14 09:15:45 -0400

    Fixed update user

--- src/main/java/com/revature/services/UserServiceImpl.java
+++ src/main/java/com/revature/services/UserServiceImpl.java
@@ -63,7 +63,7 @@ public class UserServiceImpl implements UserService {
 	public User updateProfile(User u) {
 		if (userRepo.findById(u.getUserId()) != null)
 		{
-			if (u.getAddress() != null && u.getFirstName() != null &&
+			if (u.getPersonalAddress() != null && u.getFirstName() != null &&
 				u.getLastName() != null) {
 					return userRepo.save(u);
 			}
